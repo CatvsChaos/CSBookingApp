fd61bf4000ce6dc045f94915c49d9bde48ffd378
CatvsChaos <J313287@tafe.wa.edu.au>
2025-08-19 16:44:17 +0800

    Removed weird >>>> comment

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,14 +2,10 @@
 <<<<<<< HEAD
 using System.Windows;
 using System.Windows.Controls;
-
-=======
 using System.Diagnostics.Contracts;
 using System.Windows;
 using System.Windows.Controls;
 
-
->>>>>>> 9fa45e3c10010c3bd924abed766e3e123de6e4ba
 namespace BookMyTradie
 {
 
